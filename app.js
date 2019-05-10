d9dd5256c3783b1d114acf172f20e6e7534a47cd
Thad Craft <tcraft@pivotal.io>
2019-05-10 13:42:27 -0500

    fixing faq and adding gitignore

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express'),
-    app     = express(),
-    swig    = require('swig'),
-    fs      = require('fs'),
+    app = express(),
+    swig = require('swig'),
+    fs = require('fs'),
     server;
 
 // Template Engine Setup
@@ -14,44 +14,34 @@ app.set('view cache', false);
 
 // Routes
 app.get('/', function (req, res) {
-  var vcap_app = JSON.parse(process.env.VCAP_APPLICATION);
-  res.render('index', {
-    activePage: 'hello',
-    name: vcap_app.application_name,
-    host: process.env.CF_INSTANCE_IP,
-    port: process.env.PORT,
-    index: process.env.CF_INSTANCE_INDEX
-  });
+    var vcap_app = JSON.parse(process.env.VCAP_APPLICATION);
+    res.render('index', {
+        activePage: 'hello',
+        name: vcap_app.application_name,
+        host: process.env.CF_INSTANCE_IP,
+        port: process.env.PORT,
+        index: process.env.CF_INSTANCE_INDEX
+    });
 });
 
 app.get('/tour', function (req, res) {
-  res.render('tour', {
-    activePage: 'tour'
-  });
+    res.render('tour', {
+        activePage: 'tour'
+    });
 });
 
-app.get('/links', function (req, res) {
-  res.render('links', {
-    activePage: 'links'
-  });
+app.get('/FAQ', function (req, res) {
+    res.render('FAQ', {
+        activePage: 'FAQ'
+    });
 });
-
-app.get('/crash', function () {
-  fs.readFile('somefile.txt', function (err, data) {
-    if (err) throw err;
-    console.log(data);
-  });
-});
-
-
-
 
 // Fire it up
 server = app.listen(process.env.PORT || 3000, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('ExpressApp listening at http://%s:%s', host, port);
+    console.log('ExpressApp listening at http://%s:%s', host, port);
 
 });
